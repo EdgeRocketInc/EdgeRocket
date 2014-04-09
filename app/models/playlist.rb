2ee22ece6d121f1e36555158b53527965a9a5e3d
Aleksey Dmitriyev <aleksey@dmitriyev.name>
2014-04-09 00:58:17 -0600

    more playlists work on my courses page

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,3 +1,6 @@
 class Playlist < ActiveRecord::Base
-	has_and_belongs_to_many :products
+  has_and_belongs_to_many :products
+
+  # this field can be calculated on the fly
+  attr_accessor :percent_complete
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140408050508) do
+ActiveRecord::Schema.define(version: 20140409045033) do
 
   create_table "my_courses", force: true do |t|
     t.integer  "user_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140408050508) do
     t.boolean  "mandatory"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   create_table "playlists_products", id: false, force: true do |t|
