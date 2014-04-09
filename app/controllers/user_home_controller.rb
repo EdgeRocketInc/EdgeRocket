a0d3cb4f8126691a47f01c98ab00a4a334df6ae8
Aleksey Dmitriyev <aleksey@dmitriyev.name>
2014-04-09 14:04:59 -0600

    playlists on home page

--- app/controllers/user_home_controller.rb
+++ app/controllers/user_home_controller.rb
@@ -2,5 +2,9 @@ class UserHomeController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    u = User.find_by_email(current_user.email)
+
+    # --- Playlists section
+    @my_playlists = u.playlists
   end
 end
