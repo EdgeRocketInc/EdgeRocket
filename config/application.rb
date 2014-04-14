a330d5c83aab115511445c27f247b310d6a23105
Aleksey Dmitriyev <aleksey@dmitriyev.name>
2014-04-13 21:19:14 -0600

    heroku again

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module EdgeApp
     # config.i18n.default_locale = :de
     
     # need for Twitter bootstrap and AngularJS
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif superhero.js superhero.css user_home.js)
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif superhero.js superhero.css user_home.js angular.js)
     config.assets.compile = true
 
     # need for Devise
