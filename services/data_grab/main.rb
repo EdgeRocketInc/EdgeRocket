e87acf681f31df9f4c1df9520443c1a0c2f4d411
Aleksey Dmitriyev <aleksey@dmitriyev.name>
2014-05-15 00:03:58 -0600

    data grab json

--- services/data_grab/main.rb
+++ services/data_grab/main.rb
@@ -2,16 +2,31 @@ require 'rubygems'
 require 'bundler/setup' 
 
 require 'rest_client'
+require 'active_record'
+require 'logger'
 
 class CourseraClient
  	def self.courses
- 		
+		courses = RestClient.get "https://api.coursera.org/api/catalog.v1/courses", {:accept => :json}
+		courses_json = JSON.parse(courses)
+		courses_json['elements']		
  	end
 end
 
-courses = RestClient.get "https://api.coursera.org/api/catalog.v1/courses", {:accept => :json}
-courses_json = JSON.parse(courses)
+class Product < ActiveRecord::Base
+end
 
-for crs in courses_json['elements']
+# Establish our DB connection 
+ActiveRecord::Base.establish_connection\
+	:adapter => 'sqlite3',\
+	:database => '../../web/edge_app/db/development.sqlite3'
+
+
+
+courses_json = CourseraClient.courses
+
+for crs in courses_json
+	prd = Product.new
+	prd.save
 	puts crs['name']
 end
