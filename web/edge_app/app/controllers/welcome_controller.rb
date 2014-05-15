fd256490e4e845fe17f603b83ce45f143147b9a5
Aleksey Dmitriyev <aleksey@dmitriyev.name>
2014-05-15 00:06:46 -0600

    removed content of edge_app because it is in heroku branch

--- web/edge_app/app/controllers/welcome_controller.rb
+++
@@ -1,6 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-    # TODO: add authenitcaiton such as https://github.com/plataformatec/devise
-
-  end
-end
--- web/edge_app/config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-EdgeApp::Application.config.session_store :cookie_store, key: '_edge_app_session'
