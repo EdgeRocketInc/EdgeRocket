831aa54556e7551c2bbceeef84b5f8f71a39814c
Aleksey Dmitriyev <aleksey@dmitriyev.name>
2014-05-29 18:38:22 -0600

    force password change on first login

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,13 @@ class WelcomeController < ApplicationController
   def index
     # authenitcaiton via https://github.com/plataformatec/devise
     if user_signed_in? then
-      redirect_to controller: 'user_home', action: 'index'
-    else		
+      # if user signed in first time, force them to change the password
+      if current_user.sign_in_count > 1
+      	redirect_to controller: 'user_home', action: 'index'
+      else
+      	redirect_to controller: 'devise/registrations', action: 'edit'
+      end
+    else	
       redirect_to controller: 'devise/sessions', action: 'new'
     end
   end
