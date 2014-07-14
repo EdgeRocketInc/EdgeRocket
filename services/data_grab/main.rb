6dc65a29a57ea3963ce14766c4e9980c2b2fe6d8
Aleksey Dmitriyev <aleksey@dmitriyev.name>
2014-07-14 14:04:02 -0600

    Added JSON and GA provider

--- services/data_grab/main.rb
+++ services/data_grab/main.rb
@@ -6,32 +6,142 @@ require 'active_record'
 require 'logger'
 require 'optparse'
 require 'yaml'
-#require 'byebug'
+require 'byebug'
 
-class CourseraClient
- 	def self.courses
-		courses = RestClient.get "https://api.coursera.org/api/catalog.v1/courses?fields=instructor&includes=instructors", {:accept => :json}
+class ProviderClient
+
+  attr_accessor :vendor_id
+  attr_accessor :json_data
+
+  def initialize(vendor_id, json_file)
+  	self.vendor_id = vendor_id
+  	# read json data if it's provided
+	if !json_file.nil? 
+		io = IO.read(json_file)
+		self.json_data = JSON.parse( io )
+	end
+  end
+
+  def origin(row)
+    raise "Abstract method called"
+  end
+
+  def name(row)
+    raise "Abstract method called"
+  end
+
+  def description(row)
+    raise "Abstract method called"
+  end
+
+  def price(row)
+    raise "Abstract method called"
+  end
+
+  def authors(row)
+    raise "Abstract method called"
+  end
+
+  def courses
+    raise "Abstract method called"
+  end
+
+  def instructors
+    raise "Abstract method called"
+  end
+end
+
+# using Coursera API
+class CourseraClient < ProviderClient
+
+	def origin(row)
+		course_url = 'https://www.coursera.org/course/' + row['shortName']
+	end
+
+	def name(row)
+		row['name']
+	end
+
+    def description(row)
+		row['shortDescription']
+    end
+
+    def price(row)
+    	nil
+ 	end
+
+	def authors(row)
+		row['instructor']
+	end
+
+ 	def courses
+		courses = RestClient.get "https://api.coursera.org/api/catalog.v1/courses?fields=instructor,shortDescription&includes=instructors", {:accept => :json}
 		courses_json = JSON.parse(courses)
 		courses_json['elements']		
  	end
 
- 	def self.instructors
+ 	def instructors
 		instructors = RestClient.get "https://api.coursera.org/api/catalog.v1/instructors", {:accept => :json}
 		instructors_json = JSON.parse(instructors)
 		instructors_json['elements']		
  	end
 end
 
+# Using scraped data passed as JSON
+class JsonClient < ProviderClient
+
+	def origin(row)
+		row['origin'][0]
+	end
+
+	def name(row)
+		row['name'][0]
+	end
+
+    def description(row)
+		row['description'][0]
+    end
+
+    def price(row)
+    	49
+ 	end
+
+	def authors(row)
+		row['authors'][0]
+	end
+
+	def courses
+		json_data['data']
+	end
+
+	def instructors
+		nil
+	end
+end
+
 class Product < ActiveRecord::Base
 end
 
+# it maps providers according to the value in vendors table
+providers = [
+	{ vendor_id: 1, provider_class: CourseraClient },
+	{ vendor_id: 9, provider_class: JsonClient }
+]
 
 options = {:config => nil}
 OptionParser.new do |opts|
   opts.banner = "Usage: main.rb [options]"
 
-  opts.on('-c', '--config config', 'Config file') do |config|
+  opts.on('-c', '--config config', 'Config file (required)') do |config|
     options[:config] = config
+  end
+
+  opts.on('-p', '--provider provider', 'Provider of data (required)') do |provider|
+    options[:provider] = provider
+  end
+
+  opts.on('-j', '--json input_file', 'Input File in JSON format') do |json|
+    options[:json] = json
   end
 
   opts.on('-h', '--help', 'Displays Help') do
@@ -40,7 +150,7 @@ OptionParser.new do |opts|
   end
 end.parse!
 
-if options[:config] == nil
+if options[:config].nil? || options[:provider].nil?
     puts 'Missing arguments. Use -h for help'
     exit
 end
@@ -57,19 +167,31 @@ ActiveRecord::Base.establish_connection\
 	:password => config['database']['password']
 
 
-instructors_json = CourseraClient.instructors
-courses_json = CourseraClient.courses
+# get the right provider class and create its instance
+provider = nil
+for p in providers
+	if p[:vendor_id] == options[:provider].to_i
+		provider = p[:provider_class].new(p[:vendor_id], options[:json])
+		break
+	end
+end
+
+instructors_json = provider.instructors
+courses_json = provider.courses
 skipped = 0
 
 courses_json.each_with_index { |crs, i|
 	# construct the course url and then search existing record in the DB
-	course_url = 'https://www.coursera.org/course/' + crs['shortName']
+	course_url = provider.origin(crs)
 	existing_prd = Product.find_by_origin(course_url)
 	if existing_prd.nil?
+		#byebug
 		prd = Product.new
-		prd.vendor_id = 1 # Coursera
-		prd.name = crs['name']
-		prd.authors = crs['instructor']
+		prd.vendor_id = provider.vendor_id
+		prd.name = provider.name(crs) 
+		prd.description = provider.description(crs)
+		prd.price = provider.price(crs)
+		prd.authors = provider.authors(crs) 
 		# in some cases, instructors field may be empty, then we need to dig into the assicoated links
 		if prd.authors.blank?
 			#puts "blank instr, linked instructors=" + crs['links']['instructors'].to_s
