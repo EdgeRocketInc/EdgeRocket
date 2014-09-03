7c08ee1cb0ee8311714faa5731be698ab4c3e342
Brad Scharmann <bscharm@gmail.com>
2014-09-03 16:38:32 -0600

    added test for creating user from pending user

---
+++ test/features/pending_user_test.rb
@@ -0,0 +1,46 @@
+require "test_helper"
+require "database_cleaner"
+
+DatabaseCleaner.strategy = :truncation
+
+class PendingUserTest < Capybara::Rails::TestCase
+  self.use_transactional_fixtures = false
+
+  setup do
+    DatabaseCleaner.start
+  end
+
+  teardown do
+    DatabaseCleaner.clean
+  end
+
+  test 'encrypt_password' do
+
+    Capybara.current_driver = :selenium
+
+
+    password = "password"
+    pending_user = PendingUser.new(
+      first_name: 'Admin',
+      last_name: 'Test',
+      email: 'admin-test@edgerocket.co',
+      encrypted_password: password,
+    )
+    pending_user.save
+
+    user = User.create!(
+      email: pending_user.email,
+      password: 'password'
+    )
+    user.update_column(:encrypted_password, pending_user.encrypted_password)
+
+    visit "/"
+    fill_in "user_email", with: 'admin-test@edgerocket.co'
+    fill_in "user_password", with: 'password'
+    click_button 'Sign in'
+
+    assert_content page, "Playlists"
+  end
+
+end
+
