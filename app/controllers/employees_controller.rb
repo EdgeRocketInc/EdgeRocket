67b3de63d02f1df12ee3f1d617ccddde335f93eb
Layne McNish <lmcnish14@gmail.com>
2014-09-30 14:27:37 -0600

    when an admin adds a user, the user receives an email

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -26,7 +26,7 @@ class EmployeesController < ApplicationController
       # we can add a role if it's not a regular user
       Role.insert_role(user_role, @user.id)
       Notifications.admin_adds_user_email(@user).deliver
-      Notifications.admin_adds_user_email(@user, request.host_with_port).deliver
+      # Notifications.admin_adds_user_email(@user, request.host_with_port).deliver
     end
 
     respond_to do |format|
