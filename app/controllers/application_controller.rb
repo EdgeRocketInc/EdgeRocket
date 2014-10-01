239747553e5293863e0723c2784eb683b6ddd1db
Layne McNish <lmcnish14@gmail.com>
2014-10-01 14:19:46 -0600

    added an event machine into the application controller to try to track keen io asynchronously

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  require 'em-http-request'
+
+  Thread.new { EventMachine.run }
+
   # not needed because it's included in other layouts
   # layout 'superhero'
 
