f0b5f91c8101f0bba0ffb66f046193209075536b
Aleksey Dmitriyev <aleksey@dmitriyev.name>
2014-10-02 15:50:04 -0600

    changed text on the sign in screen

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(:default, Rails.env)
 
 module EdgeApp
 
-  VERSION = '0.1.50'
+  VERSION = '0.1.51'
 
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
