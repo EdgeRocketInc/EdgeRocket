a14b6cb11e90da3828a8ffa6ccb6bce71b414ea4
Layne McNish <lmcnish14@gmail.com>
2014-10-02 20:34:05 -0600

    removed old migration to hopefully fix migration problem removing old column

--- db/migrate/20141002201029_add_disable_column_to_accounts_table.rb
+++
@@ -1,5 +0,0 @@
-class AddDisableColumnToAccountsTable < ActiveRecord::Migration
-  def change
-    add_column :accounts, :disabled, :boolean, :default=>false
-  end
-end
--- db/migrate/20141002212512_change_column_name_users_table.rb
+++ db/migrate/20141002212512_change_column_name_users_table.rb
@@ -1,7 +1,5 @@
 class ChangeColumnNameUsersTable < ActiveRecord::Migration
   def change
-    remove_column :users, :disabled
-
     add_column :users, :is_active, :boolean, :default=>true
   end
 end
