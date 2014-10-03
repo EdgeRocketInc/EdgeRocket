920c7fcf67d3d1e7a0326f8b6c2349bcfd35bd8e
Layne McNish <lmcnish14@gmail.com>
2014-10-02 20:38:13 -0600

    removed old migrations and added new one to add active column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141002212512) do
+ActiveRecord::Schema.define(version: 20141003023524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 20141002212512) do
     t.text     "options"
     t.string   "account_type"
     t.string   "domain"
-    t.boolean  "disabled",     default: false
   end
 
   add_index "accounts", ["company_name"], name: "index_accounts_on_company_name", unique: true, using: :btree
