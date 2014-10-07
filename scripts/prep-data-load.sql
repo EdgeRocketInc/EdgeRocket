0b5e67f9f24488b85fabf271a1ab68140450a759
Aleksey Dmitriyev <aleksey@dmitriyev.name>
2014-10-07 16:37:34 -0600

    data prep

---
+++ scripts/prep-data-load.sql
@@ -0,0 +1,24 @@
+-- KEEP products that are used
+
+select p.id, name, origin from products p 
+where vendor_id = 3
+-- and manual_entry='f'
+and (
+	id in (select product_id from discussions where product_id=p.id)
+	or id in (select product_id from my_courses where product_id=p.id)
+	or id in (select product_id id from playlist_items where product_id=p.id)
+	or id in (select product_id id from recommendations where product_id=p.id)
+);
+
+
+-- DELETE products that are NOT used
+select count(*) --p.id, name 
+from products p 
+where vendor_id = 3
+-- and manual_entry='f'
+and (
+	id not in (select product_id from discussions where product_id=p.id)
+	and id not in (select product_id from my_courses where product_id=p.id)
+	and id not in (select product_id id from playlist_items where product_id=p.id)
+	and id not in (select product_id id from recommendations where product_id=p.id)
+);
