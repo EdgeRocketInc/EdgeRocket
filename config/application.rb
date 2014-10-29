1c63b44f2487837299f3acdb0ae063fcb05643b1
Aleksey Dmitriyev <aleksey@dmitriyev.name>
2014-10-29 17:13:34 -0600

    logos and separators

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(:default, Rails.env)
 
 module EdgeApp
 
-  VERSION = '0.1.71'
+  VERSION = '0.1.72'
 
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
