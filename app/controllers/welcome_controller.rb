cce639ed42179f942fb362bfb529672c2db2a306
Aleksey Dmitriyev <aleksey@dmitriyev.name>
2014-11-10 10:56:49 -0700

    fixed keenio event for the welcome action

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,14 @@ class WelcomeController < ApplicationController
   def index
     # authenitcaiton via https://github.com/plataformatec/devise
     if user_signed_in? then
+
+      publish_keen_io(:html, :ui_actions, {
+          :user_signed_in => true,
+          :user_email => current_user.nil? ? nil : current_user.email,
+          :action => controller_path,
+          :method => action_name
+      })
+
       # if user signed in first time, force them to change the password
       if current_user.reset_required == true
         redirect_to controller: 'welcome', action: 'edit_password'
@@ -9,14 +17,13 @@ class WelcomeController < ApplicationController
         redirect_to controller: 'user_home', action: 'index'
       end
     else	
+      publish_keen_io(:html, :ui_actions, {
+          :user_signed_in => false,
+          :action => controller_path,
+          :method => action_name
+      })
       redirect_to controller: 'devise/sessions', action: 'new'
     end
-
-    publish_keen_io(:html, :ui_actions, {
-        :user_email => current_user.nil? : nil : current_user.email,
-        :action => controller_path,
-        :method => action_name
-    })
 
   end
 
