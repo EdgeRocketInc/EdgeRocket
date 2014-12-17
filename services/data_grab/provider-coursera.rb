3dcb0d3cc0f3c465fa5265c4403a151ebeffb2c6
Aleksey Dmitriyev <aleksey@dmitriyev.name>
2014-12-16 17:09:26 -0700

    added school to coursera

--- services/data_grab/provider-coursera.rb
+++ services/data_grab/provider-coursera.rb
@@ -1,3 +1,5 @@
+require_relative 'coursera_helper_methods'
+
 # using Coursera API
 class CourseraClient < ProviderClient
 
@@ -22,9 +24,14 @@ class CourseraClient < ProviderClient
 	end
 
  	def courses
-		courses = RestClient.get "https://api.coursera.org/api/catalog.v1/courses?fields=instructor,shortDescription,estimatedClassWorkload&includes=instructors,sessions", {:accept => :json}
-		courses_json = JSON.parse(courses)
-		courses_json['elements']		
+		courses = RestClient.get "https://api.coursera.org/api/catalog.v1/courses?fields=instructor,shortDescription,estimatedClassWorkload&includes=instructors,sessions,universities", {:accept => :json}
+		courses_json = JSON.parse(courses)    
+    
+    # retrive the list of universities to lookup as school feild later
+    all_schools = RestClient.get "https://api.coursera.org/api/catalog.v1/universities?fields=name", {:accept => :json}
+    @all_schools = JSON.parse(all_schools)['elements']
+    
+    courses_json['elements']    
   end
 
   def sessions(session_ids)
@@ -40,7 +47,13 @@ class CourseraClient < ProviderClient
  	end
 
   def school(row)
-    nil
+    s = nil
+    if row["links"] && row["links"]["universities"] # for COURSERA API
+      school_id = row["links"]["universities"][0]
+      s_obj = @all_schools.find {|u| u['id'] == school_id }
+      s = s_obj['name'] if !s_obj.nil?
+    end
+    return s
   end
 
   # combines estimated workload/week (in hours) for a course with estimated weeks for all the sessions to set duration
@@ -48,7 +61,7 @@ class CourseraClient < ProviderClient
     d = nil
     if row["links"] && row["links"]["sessions"] # for COURSERA API
       session_ids = row["links"]["sessions"].join(",")
-      sessions_json = provider.sessions(session_ids)
+      sessions_json = sessions(session_ids)
       estimated_workload = calculate_workload(row)
       total_length = get_session_weeks(sessions_json)
       d = calculate_duration(estimated_workload, total_length)
