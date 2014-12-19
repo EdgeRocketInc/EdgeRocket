bd49d58de56906ac432f10b6563ffd7a5b147e6a
Aleksey Dmitriyev <aleksey@dmitriyev.name>
2014-12-19 14:34:18 -0700

    changed js include to use https

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(:default, Rails.env)
 
 module EdgeApp
 
-  VERSION = '0.2.4'
+  VERSION = '0.2.5'
 
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
