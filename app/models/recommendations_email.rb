21bd845e8fe27ec29e9e66e328f34f630885ba3d
Aleksey Dmitriyev <aleksey@dmitriyev.name>
2014-12-23 15:46:00 -0700

    make survey history more reliable

--- app/models/recommendations_email.rb
+++ app/models/recommendations_email.rb
@@ -23,8 +23,10 @@ class RecommendationsEmail < ActiveRecord::Base
     recommendation_hash.map do |skill_id, product_id|
       # The skills or product may not be present at the database
       a_skill = Skill.find_by_id(skill_id)
-      a_product = Product.find_by_id(product_id)
-      {skill: a_skill, recommendations: a_product}
+      if !a_skill.nil?
+        products = Product.where(id: product_id)
+        {skill: a_skill, recommendations: products}
+      end
     end
   end
   
