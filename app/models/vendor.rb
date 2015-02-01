99b2d504f378981211526f3422e54d1130592b68
Aleksey Dmitriyev <aleksey@dmitriyev.name>
2015-01-31 17:16:38 -0700

    added vendors and fixed sql

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -6,8 +6,8 @@ class Vendor < ActiveRecord::Base
 
 	def self.all_count_by_product
 		self.find_by_sql [ \
-			'select v.id, v.name, count(*) as prod_count ' \
-			'from vendors v join products p on v.id=p.vendor_id ' \
+			'select v.id, v.name, count(p.id) as prod_count ' \
+			'from vendors v left outer join products p on v.id=p.vendor_id ' \
 			'group by p.vendor_id, v.name, v.id ' \
 			'order by prod_count desc' \
 		]
