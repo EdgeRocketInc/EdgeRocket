0c105cf4b7c25fd4ac1cf777a74c02b56d7b625f
Aleksey Dmitriyev <aleksey@dmitriyev.name>
2015-02-07 02:56:32 -0700

    minor cosmetic changes, version rev

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(:default, Rails.env)
 
 module EdgeApp
 
-  VERSION = '0.3.53'
+  VERSION = '0.3.54'
 
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
