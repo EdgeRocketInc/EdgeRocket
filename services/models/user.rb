36e99a0fbb5257b1faf392753d89b61ca89897bb
Aleksey Dmitriyev <aleksey@dmitriyev.name>
2015-02-10 11:58:43 -0700

    added email condition

--- services/models/user.rb
+++ services/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 	def self.all_incomplete
 		sql = "select email, count(mcc.id) " \
 			+ "from users u left join (select * from my_courses where status<>'compl') mcc on u.id=mcc.user_id " \
-			+ "where lower(email) not like '%@techcorp.com' " \
+			+ "where email_preference='all' and lower(email) not like '%@techcorp.com' " \
 			+ "group by email having count(mcc.id) > 0"
     	self.find_by_sql sql
 	end
